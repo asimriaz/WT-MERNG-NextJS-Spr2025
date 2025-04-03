58f3c22f899644e178e0d9b5936b109d4b112646
Asim Riaz <asim.riaz@live.com>
2025-04-03 22:35:47 +0500

    step-4

---
+++ src/components/DataSheet.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function DataSheet() {
+	return (
+		<div style={{ display: "flex", flexDirection: "row" }}>
+			<div style={{ flexGrow: 2 }}></div>
+			<div style={{ flexGrow: 2 }}></div>
+			<div style={{ flexGrow: 1 }}></div>
+		</div>
+	);
+}
