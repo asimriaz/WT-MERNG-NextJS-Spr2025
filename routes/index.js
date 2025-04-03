0e1d0e3714349a55dd7ef61e3861269499ba4f02
Asim Riaz <asim.riaz@live.com>
2025-04-03 23:04:30 +0500

    step-6

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 import express from 'express'
+import { db } from '../models/index.js';
 const router = express.Router();
+
+router.get("/students/:regno", async (req, res) => {
+    const student = await db.Student.findOne({ regno: req.params.regno });
+    res.status(200).json(student);
+});
 
 
 export default router
--- src/components/Student.tsx
+++ src/components/Student.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { api } from "../api";
 
 export default function Student() {
 	const handleKeyDown = async (e: React.KeyboardEvent<HTMLInputElement>) => {
 		if (e.key === "Enter") {
 			// Check if the Enter key is pressed
 			console.log(`Enter key pressed with value: ${e.currentTarget.value}`); // Log the value of the input field
+
+			const student = await (await api.get(`/api/students/${e.currentTarget.value}`)).data;
+			console.log(student);
 		}
 	};
 
@@ -14,6 +18,7 @@ export default function Student() {
 				<label>Reg # : </label>
 				<input type="text" name="regno" onKeyDown={handleKeyDown} />
 			</div>
+
 		</>
 	);
 }
